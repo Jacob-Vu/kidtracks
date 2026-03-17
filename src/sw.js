0311a0f964cdf73a04ee09971f0ba2f081a673e8
vvquyet <vvquyet@humaxdigital.com>
2026-03-17 23:15:32 +0700

    feat: force PWA auto-update with skipWaiting + clientsClaim

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,12 @@
-import { precacheAndRoute } from 'workbox-precaching'
+import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching'
+import { clientsClaim } from 'workbox-core'
+
+// Force new SW to activate immediately (no waiting)
+self.skipWaiting()
+clientsClaim()
+
+// Clean up old precache entries
+cleanupOutdatedCaches()
 
 // Precache all assets injected by vite-plugin-pwa
 precacheAndRoute(self.__WB_MANIFEST)
